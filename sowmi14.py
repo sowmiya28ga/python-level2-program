0481b841fdd39633fc66942a282da543c912bf26
sowmiya28ga <gasowmiya28@gmail.com>
2025-08-13 16:16:40 +0530

    sowmi14.py

---
+++ sowmi14.py
@@ -0,0 +1,33 @@
+print()
+for i in range(10):
+    print(i,end=", ")
+   
+print()
+for i in range(16):
+    print(i,end=", ")
+   
+
+print()
+for i in range(5,10):
+    print(i,end=", ")
+   
+
+print()
+for i in range(5,20,2):
+    print(i,end=", ")
+   
+
+print()
+for i in range(50,90,3):
+    print(i,end=", ")
+
+   
+print()
+for i in range(10,0,-1):
+    print(i,end=", ")
+  
+
+print()
+for i in range(15,5,-1):
+     print(i,end=", ")
+    
